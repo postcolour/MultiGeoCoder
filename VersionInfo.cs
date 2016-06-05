4cb2330667a445216f2b5b49ec269ba2ed877b51
postcolour <postcolour@gmail.com>
2016-06-05 18:36:20 +0300

    add docs folder and screenshot of program

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("1.0.5986.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.5986.0")]
+[assembly: System.Reflection.AssemblyVersion("1.0.6000.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.6000.0")]
 
 
